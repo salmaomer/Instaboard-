592e0e59c6caf11b1ef0cbf6670ff74091eef244
salma abusier <salma4567omer@gmail.com>
2025-07-21 22:57:49 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+/* Importing The useState Hook From React */
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+/* Importing A Child Component */
+import CounterDisplay from './Components/CounterDisplay';
 
 function App() {
+  /* This Declares A State :
+    count >> Current Value
+    setCount(newValue) >> Function To Update It 
+    initial value >> 0
+  */
+  const [count, setCount] = useState(0);
+  /*  JSX Return Block */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* Fragment To Wrap Multiple Elements */
+    <>
+      /* This Passes The Current count As A Prop To The CounterDisplay Component, Which Will Show It On The Screen. */
+      <CounterDisplay count = {count} />
+      /* A Button That Increases The Count By 1 When Clicked. */
+      <button onClick = {() => setCount(count + 1)}>Increment</button>
+      /* A Button That Resets The Count To 0 When Clicked. */
+      <button onClick = {() => setCount (0)}>Reset</button>
+      /* A Button That Decreases The Count By 1 When Clicked. */
+      <button onClick = {() => setCount(count - 1)}>Decrement</button>
+    </>
   );
 }
 
